44f68f189fa902c7dfae5e0a7f0fbcd824b93c4c
CAN GEL <can.gel@hotmail.com>
2024-03-23 03:28:16 +0300

    Favories Alert added.

--- src/components/cards/favoriesCard/favoriesCard.tsx
+++ src/components/cards/favoriesCard/favoriesCard.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Alert } from 'react-native'
 import React, { FC } from 'react'
 import { favoriEnabled, rickAndMorty } from 'assets'
 import style from './style'
@@ -6,7 +6,18 @@ import style from './style'
 interface IFavoriesCard {
     title: string
 }
+
 export const FavoriesCard: FC<IFavoriesCard> = ({ title }) => {
+    const handleIcon = () => {
+          Alert.alert('Dikkat', '... isimli karakteri favorilerden kaldırmak istediğinize emin misiniz?', [       
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+    }
     return (
 
         <View style={style.container}>
@@ -17,13 +28,13 @@ export const FavoriesCard: FC<IFavoriesCard> = ({ title }) => {
             <Text style={style.title}>
                 {title}
             </Text>
-            <TouchableOpacity onPress={()=>console.log("first")}style={style.favoriIconCotainer}>
+            <TouchableOpacity onPress={handleIcon} style={style.favoriIconCotainer}>
                 <Image
                     source={favoriEnabled}
                     style={style.favoriIcon}
                 />
             </TouchableOpacity>
-        </View>
+        </View >
 
     )
 }
